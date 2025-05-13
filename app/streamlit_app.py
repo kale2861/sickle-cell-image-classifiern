bcc1a0c2e3185dccf80ada80cdd64062fb938725
kale2861 <kale2861@gmail.com>
2025-05-13 12:14:02 -0400

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,1 +1,23 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
 
+model = load_model("../models/sickle_model.h5")
+
+st.title("🩸 Sickle Cell Disease Image Classifier")
+
+uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "png"])
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    image = image.resize((224, 224))  # adjust size based on your model's input size
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+
+    prediction = model.predict(image)
+
+    if prediction[0] > 0.5:
+        st.write("**Positive for Sickle Cell Disease**")
+    else:
+        st.write("**Negative for Sickle Cell Disease**")
